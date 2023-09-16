3ba53ffb0b6dc7479d080835191c9fa82cc272b7
Omer Lubin <omer.lubin.1337@gmail.com>
2023-09-16 16:42:07 +0300

    ADD temporary expression implementation

--- surgeon/expression/expression.py
+++ surgeon/expression/expression.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 
 @dataclass
 class Expression:
+    content: str
+
     def __post_init__(self) -> None:
         if type(self) == Expression:
             raise TypeError("Don't use this class - use a subclass.")
