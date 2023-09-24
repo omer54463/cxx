bc54c40043e932a79d23a5f1c7cf3227486fafab
Omer Lubin <omer.lubin.1337@gmail.com>
2023-09-24 20:31:31 +0300

    ADD declaration __init__ imports

---
+++ surgeon/declaration/__init__.py
@@ -0,0 +1,34 @@
+from surgeon.declaration.alias_declaration.alias_declaration import AliasDeclaration
+from surgeon.declaration.alias_declaration.alias_mode import AliasMode
+from surgeon.declaration.class_declaration.class_access import ClassAccess
+from surgeon.declaration.class_declaration.class_base import ClassBase
+from surgeon.declaration.class_declaration.class_declaration import ClassDeclaration
+from surgeon.declaration.class_declaration.class_declaration_block import (
+    ClassDeclarationBlock,
+)
+from surgeon.declaration.class_declaration.class_definition import ClassDefinition
+from surgeon.declaration.declaration import Declaration
+from surgeon.declaration.enum_declaration.enum_declaration import EnumDeclaration
+from surgeon.declaration.enum_declaration.enum_definition import EnumDefinition
+from surgeon.declaration.enum_declaration.enum_member import EnumMember
+from surgeon.declaration.function_declaration.constructor_initializer import (
+    ConstructorInitializer,
+)
+from surgeon.declaration.function_declaration.function_argument import FunctionArgument
+from surgeon.declaration.function_declaration.function_declaration import (
+    FunctionDeclaration,
+)
+from surgeon.declaration.function_declaration.function_definition import (
+    FunctionDefinition,
+)
+from surgeon.declaration.namespace_declaration.namespace_declaration import (
+    NamespaceDeclaration,
+)
+from surgeon.declaration.namespace_declaration.namespace_definition import (
+    NamespaceDefinition,
+)
+from surgeon.declaration.simple_declaration.simple_declaration import SimpleDeclaration
+from surgeon.declaration.simple_declaration.simple_definition import SimpleDefinition
+from surgeon.declaration.static_assert_declaration import StaticAssertDeclaration
+from surgeon.declaration.using_declaration.using_declaration import UsingDeclaration
+from surgeon.declaration.using_declaration.using_mode import UsingMode
